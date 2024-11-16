5a784a7b241844807905c46928351fb4c9d4a1c8
Wallisson Santos <wallyssonsantos@yahoo.com.br>
2024-11-16 18:55:46 -0300

    Tratamento de Exceções em APIs REST

---
+++ claro-dev-week-2024/src/main/java/me/dio/Application.java
@@ -0,0 +1,19 @@
+package me.dio;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
+@SpringBootApplication
+@EntityScan(basePackages = "me.dio.domain.model")
+
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
